b2505510e2ffed1237343cbeef41192f7d6e1a45
Marco Ripamonti <m.ripamonti@dkr.srl>
2023-12-09 22:29:11 +0100

    Test line txt file

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -3,13 +3,15 @@ import fs from "fs/promises";
 import path from "path";
 
 export async function GET() {
-  const file = await fs.readFile(
-    path.resolve(process.cwd(), "public", "text.txt"),
-    {
-      encoding: "utf-8",
-    }
+  //   const file = await fs.readFile(
+  //     path.resolve(process.cwd(), "public", "text.txt"),
+  //     {
+  //       encoding: "utf-8",
+  //     }
+  //   );
+  const iterator = tokenize(
+    "From that day on, Sir Arthur continued to protect the kingdom, but he never forgot the magical adventure that had taught him the true meaning of courage and love for nature. And so, his legend lived on, inspiring generations to come."
   );
-  const iterator = tokenize(file);
   const stream = iteratorToStream(iterator);
 
   return new Response(stream);
