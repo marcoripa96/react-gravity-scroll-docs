ea758524c2e8d52b9b680849d211f7b1fc22efc0
Marco Ripamonti <m.ripamonti@dkr.srl>
2023-12-09 22:20:01 +0100

    Move static file in public folder

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -2,7 +2,7 @@ import { iteratorToStream, tokenize } from "@/lib/stream/buildStream";
 import fs from "fs";
 
 export function GET() {
-  const file = fs.readFileSync(process.cwd() + "/tmp/text.txt", {
+  const file = fs.readFileSync(process.cwd() + "/public/text.txt", {
     encoding: "utf-8",
   });
   const iterator = tokenize(file);
