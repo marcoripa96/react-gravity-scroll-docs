3f9673912c509b1de1c1049c733c7e76c3f012f0
Marco Ripamonti <m.ripamonti@dkr.srl>
2023-12-09 22:32:44 +0100

    Add edge runtime

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -2,17 +2,19 @@ import { iteratorToStream, tokenize } from "@/lib/stream/buildStream";
 import fs from "fs/promises";
 import path from "path";
 
-export async function GET() {
-  //   const file = await fs.readFile(
-  //     path.resolve(process.cwd(), "public", "text.txt"),
-  //     {
-  //       encoding: "utf-8",
-  //     }
-  //   );
-  //   const iterator = tokenize(
-  //     "From that day on, Sir Arthur continued to protect the kingdom, but he never forgot the magical adventure that had taught him the true meaning of courage and love for nature. And so, his legend lived on, inspiring generations to come."
-  //   );
-  //   const stream = iteratorToStream(iterator);
+export const config = {
+  runtime: "edge",
+};
 
-  return new Response("Hello, world!");
+export async function GET() {
+  const file = await fs.readFile(
+    path.resolve(process.cwd(), "public", "text.txt"),
+    {
+      encoding: "utf-8",
+    }
+  );
+  const iterator = tokenize(file);
+  const stream = iteratorToStream(iterator);
+
+  return new Response(stream);
 }
