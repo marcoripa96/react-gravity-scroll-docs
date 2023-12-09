e45f7e09818a0ebd342ad301327352198c1574af
Marco Ripamonti <m.ripamonti@dkr.srl>
2023-12-09 22:16:07 +0100

    Move static file in tmp folder

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -2,7 +2,7 @@ import { iteratorToStream, tokenize } from "@/lib/stream/buildStream";
 import fs from "fs";
 
 export function GET() {
-  const file = fs.readFileSync("./src/app/(components)/text.md", {
+  const file = fs.readFileSync("/tmp/text.md", {
     encoding: "utf-8",
   });
   const iterator = tokenize(file);
