1bcfdd456834034e27b6dbb8c385d89a270a01ec
Marco Ripamonti <m.ripamonti@dkr.srl>
2023-12-09 23:19:21 +0100

    Convert to fake streaming

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -1,11 +1,6 @@
-import { iteratorToStream, tokenize } from "@/lib/stream/buildStream";
 import fs from "fs/promises";
 import { NextResponse } from "next/server";
 import path from "path";
-
-// export const runtime = "edge";
-
-// const text
 
 export async function GET() {
   const file = await fs.readFile(
@@ -14,8 +9,6 @@ export async function GET() {
       encoding: "utf-8",
     }
   );
-  //   const iterator = tokenize(file);
-  //   const stream = iteratorToStream(iterator);
 
   return NextResponse.json({
     text: file,
